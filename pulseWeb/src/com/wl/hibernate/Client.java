cc79ecc176bfe7b1f53166bb7b57230e9f8c22a1
wuleihenbang <wuleihenbang@126.com>
2014-03-27 20:29:22 +0800

    添加client

---
+++ pulseWeb/src/com/wl/hibernate/Client.java
@@ -0,0 +1,36 @@
+package com.wl.hibernate;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration().configure();
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = null;
+		
+		try{
+			session = factory.openSession();
+			session.beginTransaction();
+			User user = new User();
+			user.setName("张三");
+			user.setPassword("123");
+			System.out.println(new Date());
+			user.setCreateTime(new Date());
+			user.setExpireTime(new Date());
+			
+			session.save(user);
+			session.getTransaction().commit();
+		}catch(Exception e){
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		}
+
+	}
+
+}
